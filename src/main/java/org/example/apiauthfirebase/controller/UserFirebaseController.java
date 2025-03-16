5bfede9eb3c1e6e5414a54a0d2f75b8ab25cdb36
rances <rances.pardo1@gmail.com>
2025-03-16 00:48:57 -0500

    update to project KillData

--- src/main/java/org/example/apiauthfirebase/controller/UserFirebaseController.java
+++ src/main/java/org/example/apiauthfirebase/controller/UserFirebaseController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5002","https://paneladmin-a0d5c.web.app"})
+@CrossOrigin(origins = {"https://panel-admin-kill-data.web.app"})
 @RestController
 @RequestMapping("/api/user")
 public class UserFirebaseController {
